560bd11dca7703ec5debf8fa3ba8ea3367ee6eb6
nassarao <nassarao@mail.uc.edu>
2016-11-13 14:39:36 -0500

    Addded layout bundles

--- SchedBot/App_Start/BundleConfig.cs
+++ SchedBot/App_Start/BundleConfig.cs
@@ -25,6 +25,31 @@ namespace SchedBot
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/plugins").Include(
+                "~/Scripts/js.cookie.min.js",
+                "~/Scripts/jquery.slimscroll.min.js",
+                "~/Scripts/jquery.blockui.min.js",
+                "~/Scripts/jquery.uniform.min.js",
+                "~/Scripts/bootstrap-switch.min.js",
+                "~/Scripts/bootstrap-hover-dropdown.min.js",
+                "~/Scripts/moment.min.js",
+                "~/Scripts/fullcalendar.js",
+                "~/Scripts/jquery-ui.min.js",
+                "~/Scripts/calendar.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/globalscripts").Include(
+                "~/Scripts/app.min.js",
+                "~/Scripts/ui-blockui.min.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/layoutscripts").Include(
+                "~/Scripts/layout.min.js",
+                "~/Scripts/demo.min.js",
+                "~/Scripts/quick-sidebar.min.js"
+                ));
+
+
             bundles.Add(new StyleBundle("~/Content/css", "http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/font-awesome.css",
@@ -33,14 +58,23 @@ namespace SchedBot
                       "~/Content/bootstrap-switch.css",
                       "~/Content/components.css",
                       "~/Content/plugins.css",
-                      "~/Content/Site.css"));
+                      "~/Content/Site.css",
+                      "~/Content/fullcalendar.css"));
 
             bundles.Add(new StyleBundle ("~/Content/Login/css").Include(
                  "~/Content/login.css",
                  "~/Content/select2.css",
                  "~/Content/select2-bootstrap.css"
                 ));
+
+            bundles.Add(new StyleBundle("~/Content/Layout/css").Include(
+                "~/Content/layout.min.css",
+                "~/Content/grey.min.css",
+                "~/Content/custom.min.css"));
+
         }
+
+        
 
        
     }
