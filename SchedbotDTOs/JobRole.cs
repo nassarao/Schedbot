6b0595be8d52790603de8e8b49ab3d483196352c
nassarao <nassarao@mail.uc.edu>
2016-11-26 14:28:38 -0500

    JobRoles table added

---
+++ SchedbotDTOs/JobRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedbotDTOs
+{
+    public class JobRole
+    {
+
+        public int JobRoleId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
